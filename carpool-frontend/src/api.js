5fb41d6533ac52f1621d3c9b019e99577ed40ece
shrihari-joshi <joshishrihari13@gmail.com>
2025-05-19 20:24:08 +0530

    deploy ready

--- carpool-frontend/src/api.js
+++ carpool-frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Adjust the base URL as needed
+    baseURL: process.env.REACT_APP_API_URL, // Adjust the base URL as needed
     headers: {
         'Content-Type': 'application/json',
     },
