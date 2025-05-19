0ad88fecfba11ab8fe5a032023bfe379db7e2fe2
shrihari-joshi <joshishrihari13@gmail.com>
2025-05-19 21:17:50 +0530

    cors solved -v4

--- carpool-backend/config/allowedOrigin.js
+++ carpool-backend/config/allowedOrigin.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'https://carpool-backend.onrender.com',
-    'https://wheelmates-carpooling.netlify.app', //frontend
+    'https://wheelmates-carpooling.netlify.app/api', //frontend
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000'
